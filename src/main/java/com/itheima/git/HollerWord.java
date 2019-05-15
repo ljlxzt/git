06b09393adaa8d9ad80820f23eee645d749ab831
luojialiang <www.1208060625@qq.com>
2019-05-15 21:17:41 +0800

    第一次提交项目

---
+++ src/main/java/com/itheima/git/HollerWord.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+/**
+ * @author ${AUTHOR}
+ * @data 2019/5/15 21:12
+ **/
+public class HollerWord {
+    public static void main(String[] args) {
+        System.out.println("即将获取第一次连接");
+
+    }
+}
